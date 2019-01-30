0de905dec73a0103c5535b639ec2348f74e3d5f7
Namrata <namrataramnivas.dayma@cognizant.com>
2019-01-30 15:46:05 +0530

    Initial Commit

---
+++ DemoApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DemoApp
+//
+//  Created by Pepsico_1 on 1/30/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DemoApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DemoApp
+//
+//  Created by Pepsico_1 on 1/30/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
